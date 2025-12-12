14f3875cbce2077292368a9988cb45258d9438d4
lmj3272 <im4476257@gmail.com>
2025-12-12 13:41:24 +0900

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ltvcal/',
+  base: './', // 중요: 현재 위치를 기본으로 설정
+  build: {
+    outDir: 'dist',
+  },
   define: {
-    'process.env': {}
+    'process.env': {} // 에러 방지용 안전장치
   }
 });
